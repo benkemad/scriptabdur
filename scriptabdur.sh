ca03f7a730024e995bf0481fd0b77cc955ece9c9
benkemad <70264888+benkemad@users.noreply.github.com>
2020-11-30 20:47:10 +0700

    scriptabdur.sh

--- scriptabdur.sh
+++ scriptabdur.sh
@@ -806,7 +806,7 @@ cd
 apt-get install -y libxml-parser-perl
 
 # banner /etc/issue.net
-wget -O /etc/issue.net "https://raw.githubusercontent.com/benkemad/scriptbagus/master/issue.net"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/benkemad/scriptabdur/master/issue.net"
 sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 
